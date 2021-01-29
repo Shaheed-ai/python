2c91a3a025dcdb468e02b41e4b792b58c456d210
Shaheed-ai <77835177+Shaheed-ai@users.noreply.github.com>
2021-01-29 12:32:46 -0800

    Update bus_fare_challenge.py

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,1 +1,26 @@
 # WRITE YOUR CODE SOLUTION HERE
+import datetime
+today = datetime.date.today()
+print(today)
+day=(datetime.date(2021,1,29).strftime('%a'))
+print(day)
+
+import datetime
+
+x = datetime.datetime.now().strftime("%A")
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+if x in days:
+    fare = 100
+
+else:
+    if x == "Saturday":
+        fare = 60
+
+    if x == "Sunday":
+        fare = 80
+
+print(fare)
+
+
+
